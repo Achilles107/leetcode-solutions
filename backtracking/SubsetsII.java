35a201f84d35e6ef45610ffd857fb03938aa7fcf
Achilles107 <abhinavshukla835@gmail.com>
2023-07-10 18:38:06 -0700

    SubsetsII solution

--- backtracking/SubsetsII.java
+++ backtracking/SubsetsII.java
@@ -1,35 +1,39 @@
 package backtracking;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SubsetsII {
-    private final List<List<Integer>> outer = new ArrayList<>();
-    Map<String, List<Integer>> maps = new HashMap<>();
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        findSubs(0, nums, new ArrayList<>(), "");
-        return outer;
+    private final List<List<Integer>> result = new ArrayList<>();
+
+    private void trace(int nums[], int n, List<Integer> subset, int start, Map<Integer, Integer> count){
+        result.add(new ArrayList<>(subset));
+        for (int i=start; i<n; i++){
+            if (count.get(nums[i]) > 0) {
+                subset.add(nums[i]);
+                count.put(nums[i], count.get(nums[i]) - 1);
+                trace(nums, n, subset, i, count);
+                subset.remove(subset.size()-1);
+                count.put(nums[i], count.get(nums[i]) + 1);
+            }
+        }
     }
-
-    private void findSubs(int indx, int nums[], ArrayList<Integer> subset, String str){
-        if (!str.equals("") && !maps.containsKey(str)) {
-            outer.add(subset);
-            maps.put(str, new ArrayList<>(subset));
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int n: nums){
+            count.put(n, count.getOrDefault(n, 0)+1);
         }
-
-
-        for (int i =indx; i<nums.length; i++){
-            subset.add(nums[i]);
-            str += nums[i];
-            findSubs(i+1, nums, new ArrayList<>(subset), str);
-            subset.remove(subset.size()-1);
-            str = str.substring(0, str.length()-1);
-
+        int subs[] = new int[count.size()];
+        int i =0;
+        for (Integer key: count.keySet()){
+            subs[i] = key;
+            i++;
         }
+        trace(subs, subs.length, new ArrayList<>(), 0, count);
+        return result;
     }
     public static void main(String[] args) {
-
+        int nums[] = {1,2,2,2};
+        System.out.println(new SubsetsII().subsetsWithDup(nums));
     }
 }
