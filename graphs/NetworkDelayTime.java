c268e1b037b8a491dba371eef047957c5528668e
Achilles107 <abhinavshukla835@gmail.com>
2023-07-23 17:23:19 -0700

    NetworkDelayTime solutions

--- graphs/NetworkDelayTime.java
+++ graphs/NetworkDelayTime.java
@@ -3,47 +3,56 @@ package graphs;
 import java.util.*;
 
 public class NetworkDelayTime {
-    public int networkDelayTime(int[][] times, int n, int k) {
-        int min_values_array[] = new int[n+1];
-        Arrays.fill(min_values_array, Integer.MAX_VALUE);
-
-        HashMap<Integer, List<int[]>> networktime = new HashMap<>();
-        for (int i=1; i<=n; i++){
-            networktime.put(i, new ArrayList<>());
+    private Map<Integer, List<int[]>> networks(int[][] times, int n) {
+        Map<Integer, List<int[]>> networks = new HashMap<>();
+        for(int node =1; node<=n; node++) {
+            networks.put(node, new ArrayList<int[]>());
         }
-        for (int i =0; i<times.length; i++){
-            int targets[] = {times[i][1], times[i][2]};
-            networktime.get(times[i][0]).add(targets);
+        for (int time[]: times) {
+            networks.get(time[0]).add(new int[]{time[1], time[2]});
         }
-
-        bfs(k, networktime, min_values_array);
-        int result = Integer.MIN_VALUE;
-        for (int i =1; i<min_values_array.length; i++){
-            result = Math.max(result, min_values_array[i]);
-            if (result == Integer.MAX_VALUE)
-                return -1;
-        }
-        return result;
+        return networks;
     }
 
-    private void bfs(int k, HashMap<Integer, List<int[]>> networktime, int[] minValuesArray) {
+    private void sendSignal(Map<Integer, List<int[]>> networks, int minValuesToReach[], int k) {
         Queue<Integer> queue = new LinkedList<>();
-
         queue.add(k);
-        minValuesArray[k] = 0;
+        minValuesToReach[k] = 0;
 
         while (!queue.isEmpty()) {
-            int currNode = queue.poll();
-            List<int[]> networks = networktime.get(currNode);
-            for (int i =0; i<networks.size(); i++){
-                int childNode = networks.get(i)[0];
-                minValuesArray[childNode] = Math.min(minValuesArray[childNode], minValuesArray[currNode] + networks.get(i)[1]);
-                queue.add(childNode);
+            int currVal = queue.poll();
+            if (!networks.containsKey(currVal))
+                continue;
+            List<int[]> neighbouringNodes = networks.get(currVal);
+            for (int[] node: neighbouringNodes) {
+                int neighNode = node[0];
+                int currTime = node[1];
+                int prevVal = minValuesToReach[neighNode];
+                minValuesToReach[neighNode] = Math.min(minValuesToReach[neighNode], minValuesToReach[currVal] + currTime);
+                if (prevVal > minValuesToReach[neighNode])
+                    queue.add(neighNode);
             }
         }
     }
+    public int networkDelayTime(int[][] times, int n, int k) {
+        // created network connections
+        Map<Integer, List<int[]>> networks = networks(times, n);
+        int minValuesToReach[] = new int[n+1];
+        Arrays.fill(minValuesToReach, Integer.MAX_VALUE);
+        sendSignal(networks, minValuesToReach, k);
+        int networkDelayTime = 0;
+        for (int time = 1; time<minValuesToReach.length; time++) {
+            if (minValuesToReach[time] == Integer.MAX_VALUE)
+                return -1;
+            networkDelayTime = Math.max(networkDelayTime, minValuesToReach[time]);
+        }
+        return networkDelayTime;
+    }
 
     public static void main(String[] args) {
+        int[][] times = { {2, 1, 1}, {2, 3, 1}, {3, 4, 1} };
+        int n = 4;
+        int k = 2;
 
     }
 }
