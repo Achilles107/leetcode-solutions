4c565ee5f9853f1669495be3b526c5095688c0e9
Achilles107 <abhinavshukla835@gmail.com>
2023-09-27 21:56:43 -0400

    SwappingNodesinaLinkedList solution

---
+++ linked_list/SwappingNodesinaLinkedList.java
@@ -0,0 +1,29 @@
+package linked_list;
+
+public class SwappingNodesinaLinkedList {
+
+    public ListNode swapNodes(ListNode head, int k) {
+        int listLength = 0;
+        ListNode currentNode = head;
+        // find the length of linked list
+        while (currentNode != null) {
+            listLength++;
+            currentNode = currentNode.next;
+        }
+        // set the front node at kth node
+        ListNode frontNode = head;
+        for (int i = 1; i < k; i++) {
+            frontNode = frontNode.next;
+        }
+        //set the end node at (listLength - k)th node
+        ListNode endNode = head;
+        for (int i = 1; i <= listLength - k; i++) {
+            endNode = endNode.next;
+        }
+        // swap the values of front node and end node
+        int temp = frontNode.val;
+        frontNode.val = endNode.val;
+        endNode.val = temp;
+        return head;
+    }
+}
