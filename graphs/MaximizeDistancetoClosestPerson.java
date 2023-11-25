65086d3b75329072effe403595ad72adfb94b5b5
Achilles107 <abhinavshukla835@gmail.com>
2023-11-24 21:25:03 -0500

    MaximizeDistancetoClosestPerson solution

--- graphs/MaximizeDistancetoClosestPerson.java
+++ graphs/MaximizeDistancetoClosestPerson.java
@@ -4,20 +4,18 @@ public class MaximizeDistancetoClosestPerson {
 
     private int[] onlyOnePerson(int[] seats, int n) {
         int left = 0;
-        int right = n-1;
-        boolean foundLeft= false;
+        int right = n - 1;
+        boolean foundLeft = false;
         boolean foundRight = false;
         while (left <= right) {
             if (seats[left] != 1) {
                 left++;
-            }
-            else {
+            } else {
                 foundLeft = true;
             }
             if (seats[right] != 1) {
                 right--;
-            }
-            else {
+            } else {
                 foundRight = true;
             }
             if (foundLeft && foundRight)
@@ -25,13 +23,14 @@ public class MaximizeDistancetoClosestPerson {
         }
         return new int[]{left, right};
     }
+
     public int maxDistToClosest(int[] seats) {
         int n = seats.length;
         int dist[] = onlyOnePerson(seats, n);
         int firstOne = dist[0];
         int lastOne = dist[1];
-        int maxDist = Math.max(firstOne, (n-1)-lastOne);
-        if (firstOne == lastOne){
+        int maxDist = Math.max(firstOne, (n - 1) - lastOne);
+        if (firstOne == lastOne) {
             return maxDist;
         }
         if (firstOne > lastOne)
@@ -43,22 +42,22 @@ public class MaximizeDistancetoClosestPerson {
         while (j <= lastOne) {
             if (seats[j] == 1) {
                 if (zeroesBetween % 2 == 0) {
-                    maxDist = Math.max(maxDist, zeroesBetween/2);
+                    maxDist = Math.max(maxDist, zeroesBetween / 2);
                 } else {
-                    maxDist = Math.max(maxDist, zeroesBetween/2 + 1);
+                    maxDist = Math.max(maxDist, zeroesBetween / 2 + 1);
                 }
                 firstOne = j;
                 zeroesBetween = 0;
-            }
-            else {
+            } else {
                 zeroesBetween++;
             }
             j++;
         }
         return maxDist;
     }
+
     public static void main(String[] args) {
-        int seats[] = {0,1,0,0,0,0,0,0,0,0,0,1};
+        int seats[] = {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
         System.out.println(new MaximizeDistancetoClosestPerson().maxDistToClosest(seats));
     }
 }
